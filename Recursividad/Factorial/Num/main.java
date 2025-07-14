a1d5a70022b46f84e95625d45f104b0ff7604de1
Yerson Bautista <yersonbautista123@gmail.com>
2025-07-13 19:47:18 -0500

    Realizo el metodo factorial con ciclo while

--- Recursividad/Factorial/Num/main.java
+++ Recursividad/Factorial/Num/main.java
@@ -8,10 +8,10 @@ public class main {
         // enteros positivos desde 1 hasta n.
         // Factorial de 5: 5! = 5 * 4 * 3 * 2 * 1 = 120
         // El factorial de 0 se define como 1: 0! = 1
-
         Factorial factorial = new Factorial();
 
         System.out.println("Factorial de 5 : " + "\n" + factorial.recursiveFactorial(5)); // 120
+        System.out.println("Factorial de 6 : " + "\n" + factorial.factorialCycle(6)); // 120
 
     }
 
